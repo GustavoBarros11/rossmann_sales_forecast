f062114d1e32b1725d20fad83396fd21fe6dd82f
GustavoBarros11 <gustavosb1156@gmail.com>
2022-12-30 22:05:33 -0300

    solved API building in production issues # 4

--- api/handler.py
+++ api/handler.py
@@ -48,5 +48,5 @@ def health_check():
     return Response( '{}', status=200 )
 
 if __name__ == '__main__':
-    port = os.envion.get('PORT', 5000)
+    port = os.environ.get('PORT', 5000)
     app.run(host='0.0.0.0', port=port)
